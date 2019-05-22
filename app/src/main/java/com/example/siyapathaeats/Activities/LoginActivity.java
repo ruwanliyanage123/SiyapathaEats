c59d9506a65f8e3b71e38b93337d215e75d5b5e8
ruwanliyanage <ruwanliyanagegalle1994@gmail.com>
2019-05-22 10:55:47 +0530

    add onStart function

--- app/src/main/java/com/example/siyapathaeats/Activities/LoginActivity.java
+++ app/src/main/java/com/example/siyapathaeats/Activities/LoginActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.siyapathaeats.R;
@@ -14,6 +15,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -21,6 +23,7 @@ public class LoginActivity extends AppCompatActivity {
     private Button btnLogin;
     private FirebaseAuth mAuth;
     private Intent HomeActivity;
+    private ImageView loginPhoto;
 
 
     @Override
@@ -33,6 +36,16 @@ public class LoginActivity extends AppCompatActivity {
         btnLogin = findViewById(R.id.login_btn);
         mAuth = FirebaseAuth.getInstance();
         HomeActivity = new Intent(this, com.example.siyapathaeats.Activities.HomeActivity.class);
+        loginPhoto  = findViewById(R.id.login_photo);
+
+        loginPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent registerActivity = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(registerActivity);
+                finish();
+            }
+        });
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -76,5 +89,17 @@ public class LoginActivity extends AppCompatActivity {
 
     private void showMessage(String text) {
         Toast.makeText(getApplicationContext(), text, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser user = mAuth.getCurrentUser();
+
+        if(user!=null){
+            //user is already connected so we redirect him to home page
+            updateUI();
+
+        }
     }
 }

7d39831c0c0a461e19f14fb54de7117c8fb35678
ruwanliyanage <ruwanliyanagegalle1994@gmail.com>
2019-05-22 09:33:10 +0530

    make function for login

--- app/src/main/java/com/example/siyapathaeats/Activities/LoginActivity.java
+++ app/src/main/java/com/example/siyapathaeats/Activities/LoginActivity.java
@@ -2,14 +2,47 @@ package com.example.siyapathaeats.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.siyapathaeats.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private EditText userMail, userPassword;
+    private Button btnLogin;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        userMail = findViewById(R.id.login_mail);
+        userPassword = findViewById(R.id.login_password);
+        btnLogin = findViewById(R.id.login_btn);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btnLogin.setVisibility(View.INVISIBLE);
+                final String mail = userMail.getText().toString();
+                final String password = userPassword.getText().toString();
+
+                if(mail.isEmpty()|| password.isEmpty()){
+                    showMessage("please verify all feilds");
+                }
+                else{
+                    signIn(mail,password);
+                }
+
+            }
+        });
+
+    }
+
+    private void showMessage(String text) {
+        Toast.makeText(getApplicationContext(), text, Toast.LENGTH_LONG).show();
     }
 }
